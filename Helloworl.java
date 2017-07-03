ec0e89af49694ed685c92797709a37f97fd487f1
Suriyarithik <suriyavedsum@gmail.com>
2017-07-03 15:42:24 +0530

    Create Helloworl.java

---
+++ Helloworl.java
@@ -0,0 +1,15 @@
+# Helloworl
+import java.io.*;
+import java.util.*;
+public class Helloworl
+{
+public static void main(String arg[])throws IOException
+{
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+String n=br.readLine();
+for(i=0;i<=n;i++)
+{
+System.out.println("Hello");
+}
+}
+}
